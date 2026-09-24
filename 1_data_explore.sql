810f93681e9abc3f214d51dceba8438edfb06b2d
AndresSebas15 <andressebastian.diazdejoy.2@gmail.com>
2026-09-23 19:34:05 -0500

    feat: data explore limited to 10 rows

---
+++ 1_data_explore.sql
@@ -0,0 +1,23 @@
+SELECT *
+FROM campanas
+LIMIT 10;
+-- 
+
+SELECT *
+FROM productos
+LIMIT 10;
+
+--
+SELECT *
+FROM productos_categorias
+LIMIT 10;
+
+--
+SELECT *
+FROM territorios
+LIMIT 10;
+
+--
+SELECT *
+FROM campanas
+LIMIT 10;
